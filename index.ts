997556303c539ded85d319afd9d7831e251ef8ca
naufalhanif25 <naufal.hanif2525@gmail.com>
2025-11-28 12:03:48 +0700

    Add index.ts

---
+++ index.ts
@@ -0,0 +1,8 @@
+import { HRON } from "./src/hron";
+
+// HRON object
+const hron = new HRON();
+const file = Bun.file("public/example.hron");
+
+// Parsing HRON into an JavaScript Object
+console.log(hron.parse(await file.text()));

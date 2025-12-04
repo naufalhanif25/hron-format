bed3c94f8121c994b5d5c0ef1fc632723f5465d0
naufalhanif25 <naufal.hanif2525@gmail.com>
2025-12-04 22:10:27 +0700

    Add tsup.config.ts

---
+++ tsup.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig({
+    format: ["cjs", "esm"],
+    entry: ["./index.ts"],
+    dts: true,
+    shims: true,
+    skipNodeModulesBundle: true,
+    clean: true,
+    sourcemap: true,
+    minify: false
+})

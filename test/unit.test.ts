4b49c69521f606fa5de5c769e17128b5fc926c4e
naufalhanif25 <naufal.hanif2525@gmail.com>
2025-12-04 20:37:42 +0700

    test: add 2 expects

--- test/unit.test.ts
+++ test/unit.test.ts
@@ -5,10 +5,18 @@ import { hron } from "../src/hron";
 // test the parse method
 test("Parse HRON string into JavaScript object", async () => {
     const file = Bun.file("public/example.hron");
-    expect(typeof hron.parse(await file.text())).toBe("object");
+    const result = hron.parse(await file.text());
+
+    expect(typeof result).toBe("object");
+    expect(JSON.stringify(data) === JSON.stringify(result)).toBe(true);
 });
 
 // test the stringify method
-test("Converts JavaScript object into HRON string", () => {
-    expect(typeof hron.stringify(data)).toBe("string");
+test("Converts JavaScript object into HRON string", async () => {
+    const file = Bun.file("public/example.hron");
+    const result = hron.stringify(data, { indent: 0, colorize: false });
+    const removeWhitespave = (input: string): string => input.replace(/ /g, "").replace(/\n/g, "");
+
+    expect(typeof result).toBe("string");
+    expect(removeWhitespave(await file.text()) === removeWhitespave(result)).toBe(true);
 });
